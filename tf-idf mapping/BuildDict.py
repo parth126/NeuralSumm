583b900d1425b2da34f699196d8583d9b5b63964
parth <parth@parth-pc>
2018-01-08 19:51:37 +0530

    Updated tf-idf script

--- tf-idf mapping/BuildDict.py
+++ tf-idf mapping/BuildDict.py
@@ -1,9 +1,10 @@
 import math
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 class computeDF():
 
-   int document_count 
-   int sentence_count
+   document_count = 0
+   sentence_count = 0
    sf = {}
    df = {}
 
@@ -15,16 +16,16 @@ class computeDF():
          self.sentence_count += 1
          for term in set(sentence):
             doc.add(term)
-            self.sf.get(term,0) += 1
+            self.sf[term] = self.sf.get(term,0) + 1
 
       for term in doc:
-         self.df.get(term,0) += 1
+         self.df[term] = self.df.get(term,0) + 1
       return 0
 
    def get_idf(self):
       idf = {}
       for term in self.df:
-         idf[term] = math.log(self.document_count / (1 + self.df[term]))
+         idf[term] = math.log(self.document_count*1.0 / (1 + self.df[term]))
       return(idf)
 
    def get_isf(self):
@@ -34,19 +35,34 @@ class computeDF():
       return(isf)
 
 
-def compute_tfidf(self, sentence, idf):
+def compute_tfidf(sentence, idf):
    tf = {}
    tf_idf = {}
 
    for term in sentence:
-      tf.get(term,0) += 1
+      tf[term] = tf.get(term,0) + 1
    for term in tf:
-      tf_idf.get[term] = tf.get(term)*idf.get(term)
+      tf_idf[term] = tf.get(term)*idf.get(term)
    return(tf_idf)
 
 
+DF = computeDF()
 
+with open('D30003') as f:
+   p = f.read()
+   sentence_list = []
+   sentences = sent_tokenize(p)
+   for s in sentences:
+      words = word_tokenize(s)
+      sentence_list.append(words)
+   DF.update_df(sentence_list)
 
+   idf = DF.get_idf()
+
+   for s in sentence_list:
+      tfIdf = compute_tfidf(s, idf)
+      print(s)
+      print(tfIdf)	
 
 
 
